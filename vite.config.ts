ad8b2a3eeeb22007f2746b6cde26a9c0a1931114
abin <1655853551@qq.com>
2023-02-19 22:08:42 +0800

    add: basic dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+import Components from "unplugin-vue-components/vite"
+import AutoImport from "unplugin-auto-import/vite"
+import UnoCss from "unocss/vite"
+import IconsResolver from "unplugin-icons/resolver"
+import Icons from "unplugin-icons/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [
+		vue(),
+		UnoCss(),
+		AutoImport({
+			imports: ["vue", "vue-router"],
+		}),
+		Components({
+			dts: true,
+			extensions: ["vue", "md"],
+			include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+			resolvers: [
+				IconsResolver({
+					prefix: "",
+				}),
+			],
+		}),
+		Icons({
+			defaultClass: "inline",
+			defaultStyle: "vertical-align: sub;",
+		}),
+	],
+
+	build: {
+		rollupOptions: {
+			onwarn(warning, warn) {
+				if (warning.code === "UNUSED_EXTERNAL_IMPORT") {
+					// Use default for everything else
+					warn(warning)
+				}
+			},
+		},
+	},
 })
