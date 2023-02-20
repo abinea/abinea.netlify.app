de58e76e53d89d404c5eda26a61462426529e6d7
abin <1655853551@qq.com>
2023-02-20 18:28:18 +0800

    add: ssg

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,40 @@ import "uno.css"
 import "./styles/main.css"
 
 import App from "./App.vue"
+import autoRoutes from "pages-generated"
+import { ViteSSG } from "vite-ssg"
+import NProgress from "nprogress"
+
+import dayjs from "dayjs"
+import LocalizedFormat from "dayjs/plugin/localizedFormat"
+
+const routes = autoRoutes.map((i) => {
+	return {
+		...i,
+		alias: i.path.endsWith("/") ? `${i.path}index.html` : `${i.path}.html`,
+	}
+})
+
+const scrollBehavior = (to: any, from: any, savedPosition: any) => {
+	return savedPosition || { top: 0 }
+}
+
+export const createApp = ViteSSG(
+	App,
+	{
+		routes,
+		scrollBehavior,
+	},
+	({ router, isClient }) => {
+		dayjs.extend(LocalizedFormat)
+
+		if (isClient) {
+			router.beforeEach(() => {
+				NProgress.start()
+			})
+			router.afterEach(() => {
+				NProgress.done()
+			})
+		}
+	}
+)
